dcd19b0d51fc1654cdc49bc5de3bcf63ab8e8769
Julian Woodward <dev@vsys.co.uk>
2016-04-20 12:29:21 +0100

    added gulp browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,30 @@ var gulp = require('gulp'),
     templateCache = require('gulp-angular-templatecache'),
     ngAnnotate = require('gulp-ng-annotate'),
     babel = require('gulp-babel'),
-    watch = require('gulp-watch');
+    runSequence = require('run-sequence');
+    
+ var browserSync = require('browser-sync').create();
+   // watch = require('gulp-watch');
 
 
-gulp.task('default',['app-js','lib-js','css','html-templates']);
+gulp.task('build',['app-js','lib-js','css','html-templates']);
+  
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+  })
+});
+
+gulp.task('watch',['browser-sync'],function(){
+  
+  gulp.watch('src/app/**/*.js',['app-js']);
+  gulp.watch(['./src/css/style.css','./src/css/vis-custom.css'],['css']);
+  gulp.watch('src/**/*.html',['html-templates']);
+  
+});
+
 
 gulp.task('clean',function(callback){
     del(['./build'],{force:true}).then(function(){callback();});
@@ -18,16 +38,18 @@ gulp.task('clean',function(callback){
 
 gulp.task('app-js', function(cb){
     return gulp.src('src/app/**/*.js')
-     //   .pipe(watch('./src/app/**/*.js'))
      .pipe(sourcemaps.init())
      .pipe(babel({presets: ['es2015']}))
      .pipe(ngAnnotate())
      .pipe(concat('bundle.js'))
      .pipe(uglify())
      .pipe(sourcemaps.write('./'))
-     .pipe(gulp.dest('./build'));
- 
+     .pipe(gulp.dest('./build'))
+     .pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
 
 
 
@@ -42,7 +64,6 @@ gulp.task('lib-js', function(){
         './lib/jquery.debouncedresize/js/jquery.debouncedresize.js',
         './lib/jquery.masonry/jquery.masonry.js',
          './lib/jquery-ui/jquery-ui.js'
-
         ])
         .pipe(concat('bundle-lib.js'))
         .pipe(uglify())
@@ -51,16 +72,20 @@ gulp.task('lib-js', function(){
 
 gulp.task('css', function(cb){
     return gulp.src(['./src/css/style.css','./src/css/vis-custom.css'])
-      //  .pipe(watch('./src/css/style.css'))
         .pipe(minify())
         .pipe(concat('bundle.css'))
-        .pipe(gulp.dest('./build'));
-       // .on('end',cb);
+        .pipe(gulp.dest('./build'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
 });
 
 gulp.task('html-templates', function () {
   return gulp.src('src/**/*.html')
     .pipe(templateCache())
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
